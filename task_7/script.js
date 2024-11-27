8ffd7bd6123607d2883bd3ce27a51184127bb938
iIBonIi <bonnerwow@gmail.com>
2024-11-27 08:20:22 +0300

    Update script.js

--- task_7/script.js
+++ task_7/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Переменные
+
     const gallery = document.querySelector('.gallery');
     const items = document.querySelectorAll('.gallery-item');
     const leftArrow = document.querySelector('.arrow.left');
@@ -8,31 +8,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const totalPagesElem = document.querySelector('.total-pages');
     
     const totalItems = items.length;
-    const itemsPerPage = 4; // 3 изображения на странице
+    const itemsPerPage = 4;
     const totalPages = Math.ceil(totalItems / itemsPerPage);
   
     let currentPage = 1;
   
-    // Обновление пейджера
     function updatePager() {
         currentPageElem.textContent = currentPage;
         totalPagesElem.textContent = totalPages;
     }
   
-    // Функция для обновления слайдера
     function updateGallery() {
         const offset = (currentPage - 1) * itemsPerPage;
         gallery.style.transform = `translateX(-${offset * (100 / itemsPerPage)}%)`;
         updatePager();
         
-        // Отключаем стрелку влево, если на первой странице
         if (currentPage === 1) {
             leftArrow.disabled = true;
         } else {
             leftArrow.disabled = false;
         }
         
-        // Отключаем стрелку вправо, если на последней странице
         if (currentPage === totalPages) {
             rightArrow.disabled = true;
         } else {
@@ -40,7 +36,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
   
-    // Стрелки влево
     leftArrow.addEventListener('click', function() {
         if (currentPage > 1) {
             currentPage--;
@@ -48,7 +43,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
   
-    // Стрелки вправо
     rightArrow.addEventListener('click', function() {
         if (currentPage < totalPages) {
             currentPage++;
@@ -56,7 +50,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
   
-    // Инициализация галереи
     updateGallery();
   });
   
